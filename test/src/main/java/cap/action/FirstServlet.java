2a82918182f5db08cdfe648ac13d459addec5dfc
starlee <starlee2008@163.com>
2018-09-05 09:02:35 +0800

    用于演示servlet的使用

---
+++ test/src/main/java/cap/action/FirstServlet.java
@@ -0,0 +1,24 @@
+package cap.action;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.print("hello servlet");
+        out.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
--- test/src/main/webapp/index.jsp
+++ test/src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
+<%@ page language="java" pageEncoding="UTF-8" %>
 <html>
 <body>
-<h2>Hello World!</h2>
+<a href="first">第一个Servlet演示程序</a>
 </body>
 </html>
