b44a21e2d1416b8782e43e07ee4af1a6d511c6f4
starlee <starlee2008@163.com>
2018-09-07 09:06:27 +0800

    第一个Struts2示例演示程序

---
+++ struts1/src/main/java/cap/action/HelloWorldAction.java
@@ -0,0 +1,20 @@
+package cap.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String sayHi() {
+
+        return SUCCESS;
+    }
+}
---
+++ struts1/src/main/webapp/result.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: starlee
+  Date: 2018/9/7
+  Time: 8:58
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<s:property value="username"/>
+</body>
+</html>
